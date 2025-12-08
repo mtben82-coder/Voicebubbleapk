3fcd0345c8e167103cd759c0e8b29635a815ef68
seekumimi-dotcom <seekumimi@gmail.com>
2025-12-08 22:43:07 +0000

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.voicebubble.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 10
-        versionName = "10.0.0"
+        versionCode = 11
+        versionName = "1.0.1"
     }
 
     // Signing configuration
